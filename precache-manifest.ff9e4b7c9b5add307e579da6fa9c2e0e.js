4a86a6c4e5bd7f8323761001e459ef4062e47767
sh3hz <shehax21@gmail.com>
2020-08-30 13:47:48 +0530

    Updates

---
+++ precache-manifest.ff9e4b7c9b5add307e579da6fa9c2e0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed0e6d33bacfeef15f9ad1642b90f434",
+    "url": "/todoapp-react/index.html"
+  },
+  {
+    "revision": "b2e0b92526e18bece9ae",
+    "url": "/todoapp-react/static/css/main.7e17e202.chunk.css"
+  },
+  {
+    "revision": "1fc0e6b19a4d4d942011",
+    "url": "/todoapp-react/static/js/2.68d18731.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp-react/static/js/2.68d18731.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2e0b92526e18bece9ae",
+    "url": "/todoapp-react/static/js/main.8a3f289a.chunk.js"
+  },
+  {
+    "revision": "d22d96de7b83955660e2",
+    "url": "/todoapp-react/static/js/runtime-main.41e09d89.js"
+  }
+]);
