f8d2e61ca94f9da70aa79d3101d6719c0c81d23f
sh3hz <shehax21@gmail.com>
2020-09-04 12:53:24 +0530

    add router

---
+++ src/component/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Header.css";
+import { link, Link } from "react-router-dom";
+
+export default function Header() {
+    return (
+        <header>
+            <Link to="/">Home</Link>
+            <Link to="/About">About</Link>
+
+            
+        </header>
+    )
+}
