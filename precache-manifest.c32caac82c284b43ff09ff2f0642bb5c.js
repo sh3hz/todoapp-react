1d831ee2cd50f9a76e1a2c5c0d6186c5d9b012b3
sh3hz <shehax21@gmail.com>
2020-09-19 13:07:03 +0530

    Updates

---
+++ precache-manifest.c32caac82c284b43ff09ff2f0642bb5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "723c6ce26e8a8361d5e2f35fdadff343",
+    "url": "/todoapp-react/index.html"
+  },
+  {
+    "revision": "58f525496ec295ef9cec",
+    "url": "/todoapp-react/static/css/main.59d20d29.chunk.css"
+  },
+  {
+    "revision": "967b8d49d47096cf1672",
+    "url": "/todoapp-react/static/js/2.dcd0ee9a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todoapp-react/static/js/2.dcd0ee9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58f525496ec295ef9cec",
+    "url": "/todoapp-react/static/js/main.2363620b.chunk.js"
+  },
+  {
+    "revision": "d22d96de7b83955660e2",
+    "url": "/todoapp-react/static/js/runtime-main.41e09d89.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todoapp-react/precache-manifest.ff9e4b7c9b5add307e579da6fa9c2e0e.js"
+  "/todoapp-react/precache-manifest.c32caac82c284b43ff09ff2f0642bb5c.js"
 );
 
 self.addEventListener('message', (event) => {
